479ce360e319437ba560849736733fd98918f5ff
Kenechukwu Mokwenye <mokwenyekene1@yahoo.com>
2023-11-28 11:35:30 +0100

    Feat: Logut views

--- resources/views/layouts/user-app.blade.php
+++ resources/views/layouts/user-app.blade.php
@@ -38,6 +38,7 @@
                     <a class="dropdown-item" href="#">Settings</a>
                     <a class="dropdown-item" href="#">Activity Log</a>
                     <div class="dropdown-divider"></div>
+                    @livewire('user.logout')
                     
 
                 </div>
